8e6fa71b1a6074c0710e11b7d99fcebb535dcbb7
MissBerry01 <mwongelamorine9@gmail.com>
2023-09-19 03:58:02 -0700

    task 2, print_binary

---
+++ print_binary.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_binary - Print an unsigned integer
+ * @n: The unsigned integer to print
+ * Return: The number of characters printed
+ */
+
+int print_binary(unsigned int n)
+{
+	int printed = 0, idx, i;
+	char binary_buffer[32];
+
+	if (n == 0)
+	{
+		printed += print_char('0');
+		return (printed);
+	}
+	
+	idx = 0;
+	while (n > 0)
+	{
+		binary_buffer[index] = (n % 2) + '0';
+		n /= 2;
+		index++;
+	}
+
+	for (i = idx - 1; i >= 0; i--)
+	{
+		printed += print_char(binary_buffer[i]);
+	}
+
+	return (printed);
+}
