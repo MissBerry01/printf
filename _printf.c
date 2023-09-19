71b3c5c8ded79ea977196ee8913074b06ad9acb5
MissBerry01 <mwongelamorine9@gmail.com>
2023-09-19 15:05:02 -0700

    update

--- _printf.c
+++ _printf.c
@@ -51,100 +51,73 @@ int format_specifier(const char *format, va_list arguments)
 	short short_num;
 	long long_num;
 
-	while (*format)
+	if (*format == 'c')
+		printed_characters += print_char(va_arg(arguments, int));
+	else if (*format == 's')
+		printed_characters += print_string(va_arg(arguments, char *));
+	else if (*format == '%')
+		printed_characters += print_percent();
+
+	else if (*format == 'd' || *format == 'i')
 	{
-		if (*format == '%')
+		if (format[1] == 'l')
 		{
-			format++;
-			if (*format == '\0')
-			{
-				return (printed_characters);
-			}
-
-			width = 0;
-			if (*format >= '0' && *format <= '9')
-			{
-				while (*format >= '0' && *format <= '9')
-				{
-					width = width * 10 + (*format - '0');
-					format++;
-				}
-			}
-
-			if (*format == 'c')
-				printed_characters += print_char(va_arg(arguments, int));
-			else if (*format == 's')
-				printed_characters += print_string(va_arg(arguments, char *));
-			else if (*format == '%')
-				printed_characters += print_percent();
-
-			else if (*format == 'd' || *format == 'i')
-			{
-				if (format[1] == 'l')
-				{
-					long_num = va_arg(arguments, long);
-					printed_characters += print_positive_long_number(long_num);
-				}
-				else if (format[1] == 'h')
-				{
-					short_num = va_arg(arguments, int);
-					printed_characters += print_positive_short_number(short_num);
-				}
-				else
-				{
-					num = va_arg(arguments, int);
-					if (num < 0)
-					{
-						printed_characters += print_char('-');
-						num = -num;
-					}
-					printed_characters += print_positive_number(num);
-				}
-			}
-			else if (*format == 'b')
-			{
-				binary_num = va_arg(arguments, unsigned int);
-				printed_characters += print_binary(binary_num);
-			}
-			else if (*format == 'u')
-			{
-				unsigned_num = va_arg(arguments, unsigned int);
-				printed_characters += print_unsigned_number(unsigned_num);
-			}
-			else if (*format == 'o')
-			{
-				octal_num = va_arg(arguments, unsigned int);
-				printed_characters += print_octal_number(octal_num);
-			}
-
-
-			else if (*format == 'x' || *format == 'X')
-			{
-				hex_num = va_arg(arguments, unsigned int);
-				printed_characters += print_hex_number(hex_num, *format);
-			}
-			else if (*format == 'S')
-			{
-				str = va_arg(arguments, char *);
-				printed_characters += print_string_with_escaping(str);
-			}
-
-			else if (*format == 'p')
-			{
-				ptr = va_arg(arguments, void *);
-				printed_characters += print_pointer_address(ptr);
-			}
-			else
-			{
-				printed_characters += print_percent();
-				printed_characters += print_char(*format);
-			}
+			long_num = va_arg(arguments, long);
+			printed_characters += print_positive_long_number(long_num);
+		}
+		else if (format[1] == 'h')
+		{
+			short_num = va_arg(arguments, int);
+			printed_characters += print_positive_short_number(short_num);
 		}
 		else
 		{
-			printed_characters += print_char(*format);
+			num = va_arg(arguments, int);
+			if (num < 0)
+			{
+				printed_characters += print_char('-');
+				num = -num;
+			}
+			printed_characters += print_positive_number(num);
 		}
-		format++;
+	}
+	else if (*format == 'b')
+	{
+		binary_num = va_arg(arguments, unsigned int);
+		printed_characters += print_binary(binary_num);
+	}
+	else if (*format == 'u')
+	{
+		unsigned_num = va_arg(arguments, unsigned int);
+		printed_characters += print_unsigned_number(unsigned_num);
+	}
+	else if (*format == 'o')
+	{
+		octal_num = va_arg(arguments, unsigned int);
+		printed_characters += print_octal_number(octal_num);
+	}
+
+
+	else if (*format == 'x' || *format == 'X')
+	{
+		hex_num = va_arg(arguments, unsigned int);
+		printed_characters += print_hex_number(hex_num, *format);
+	}
+	else if (*format == 'S')
+	{
+		str = va_arg(arguments, char *);
+		printed_characters += print_string_with_escaping(str);
+	}
+
+	else if (*format == 'p')
+	{
+		ptr = va_arg(arguments, void *);
+		printed_characters += print_pointer_address(ptr);
+	}
+	else
+	{
+		printed_characters += print_percent();
+		printed_characters += print_char(*format);
 	}
 
 	return (printed_characters);
