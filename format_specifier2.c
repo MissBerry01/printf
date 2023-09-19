e1384993bb844ec647c0d9a4f9ddc82325d8e0bd
Michael Mwangi <michaelmichire2016@gmail.com>
2023-09-19 05:00:58 -0700

    Update file

--- format_specifier2.c
+++ format_specifier2.c
@@ -37,3 +37,39 @@ int _printf(const char *format, ...)
 	va_end(arguments);
 	return (printed_characters);
 }
+
+/**
+ * format_specifier2 - Handles format specifiers
+ * @format: Format specifier
+ * @arguments: Variable argument list
+ * Return: The number of chars printed
+ */
+int format_specifier2(const char *format, va_list arguments)
+{
+	int printed_characters = 0;
+	unsigned int unsigned_num, octal_num, hex_num;
+
+	if (*format == 'u')
+	{
+		unsigned_num = va_arg(arguments, unsigned int);
+		printed_characters += print_unsigned_number(unsigned_num);
+	}
+	else if (*format == 'o')
+	{
+		octal_num = va_arg(arguments, unsigned int);
+		printed_characters += print_octal_number(octal_num);
+	}
+
+	else if (*format == 'x' || *format == 'X')
+	{
+		hex_num = va_arg(arguments, unsigned int);
+		printed_characters += print_hex_number(hex_num, *format);
+	}
+	else
+	{
+		printed_characters += print_percent();
+		printed_characters += print_char(*format);
+	}
+
+	return (printed_characters);
+}
